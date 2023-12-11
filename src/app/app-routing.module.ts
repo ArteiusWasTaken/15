4f37e82bbc89015fc29779590efe5f8c995f4535
Alexander <balexander.rosas@gmail.com>
2023-12-11 05:42:24 -0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
+
+import { IndexComponent } from "./pages/index/index.component";
+import { Index2Component } from "./pages/index2/index2.component";
+import { ProfilepageComponent } from "./pages/examples/profilepage/profilepage.component";
+import { RegisterpageComponent } from "./pages/examples/registerpage/registerpage.component";
+import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: IndexComponent },
+  { path: "home2", component: Index2Component },
+  { path: "profile", component: ProfilepageComponent },
+  { path: "register", component: RegisterpageComponent },
+  { path: "landing", component: LandingpageComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    }),
+  ],
+  exports: [],
+})
+export class AppRoutingModule {}
