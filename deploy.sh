34438a1408f2a71cc89efb112db0a67ef312f439
ArteiusWasTaken <sebastiancifer@gmail.com>
2024-01-02 12:52:06 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ sudo apt-get update
 sudo apt-get install lftp
 
 # Subir la aplicación al servidor de Hostinger (ejemplo usando FTP)
-lftp -c "open -u u874328425.devployers.site,in6aTyl7N 195.35.10.185; set ssl:verify-certificate no; mirror -R dist/ /"
+lftp -c "open -u u874328425.devployers.site,in6aTyl7N 195.35.10.185; set ssl:verify-certificate no; mirror -R dist/blk-design-system-angular/ /"
