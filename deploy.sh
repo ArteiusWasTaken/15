d75fbf8168b413f597b247ace89ff6763146b643
ArteiusWasTaken <sebastiancifer@gmail.com>
2024-01-02 12:36:37 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Instalar las dependencias
+npm install -g @angular/cli@15.2.10
 npm install
 
 # Compilar la aplicación en modo de producción
 ng build --configuration production
+
+sudo apt-get update
+sudo apt-get install lftp
 
 # Subir la aplicación al servidor de Hostinger (ejemplo usando FTP)
 lftp -c "open -u u874328425.devployers.site,in6aTyl7N 195.35.10.185; set ssl:verify-certificate no; mirror -R dist/ /"
