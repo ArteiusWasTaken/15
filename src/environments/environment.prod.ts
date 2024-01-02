42838ef3a42b0edea12f8c4f5677fe30e3b59857
Alexander <balexander.rosas@gmail.com>
2024-01-02 10:48:19 -0600

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
+import Swal from "sweetalert2";
+
 export const environment = {
-  production: true
+  production: true,
 };
+
+export const backend_url = "https://devployers.tech/";
+// export const backend_url = "http://localhost:8000/";
+
+export function swalErrorHttpResponse(err) {
+  Swal.fire({
+    title: err.title,
+    icon: "error",
+    html:
+      err.status == 0
+        ? err.message
+        : typeof err.error === "object"
+        ? err.error.error_summary
+          ? err.error.error_summary
+          : err.error.message
+        : err.error,
+  });
+}
+
+export function swalSuccessHttpResponse(res) {
+  Swal.fire(res.title, "", "success");
+}
