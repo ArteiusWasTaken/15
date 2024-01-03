5c6504236f98e3a62d836cc55e34681569be0d33
Alexander <balexander.rosas@gmail.com>
2024-01-02 18:17:29 -0600

    Verificador

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import {
-  backend_url,
-  swalErrorHttpResponse,
-} from "src/environments/environment";
-import { Ticket } from "src/app/interfaces";
 
 @Component({
   selector: "app-index",
@@ -14,24 +8,9 @@ export class IndexComponent implements OnInit, OnDestroy {
   isCollapsed = true;
   isMobile: boolean;
 
-  tickets: Ticket[] = [];
-  total_tickets: number = 100000;
-  sold_tickets: number;
-
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    this.http.get<any>(`${backend_url}getBlockedTickets`).subscribe(
-      (response) => {
-        this.tickets.push(...response["tickets"]);
-
-        this.sold_tickets = this.tickets.length;
-      },
-      (error) => {
-        swalErrorHttpResponse(error);
-      }
-    );
-
     this.isMobile = window.innerWidth < 768;
 
     window.addEventListener("resize", () => {
--- src/app/pages/rifas_lobo/verificador/verificador.component.ts
+++ src/app/pages/rifas_lobo/verificador/verificador.component.ts
@@ -1,7 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-verificador",
   templateUrl: "./verificador.component.html",
 })
-export class VerificadorComponent {}
+export class VerificadorComponent implements OnInit, OnDestroy {
+  isCollapsed = true;
+  isMobile: boolean;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 768;
+
+    window.addEventListener("resize", () => {
+      this.isMobile = window.innerWidth < 768;
+    });
+
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.add("verificador-page");
+  }
+  ngOnDestroy() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("verificador-page");
+  }
+}
