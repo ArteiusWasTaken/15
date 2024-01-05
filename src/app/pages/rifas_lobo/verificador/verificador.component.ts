240aeb0135d7432a863848a0205888e24dd33825
Adrian <sauladrian.arias@gmail.com>
2024-01-05 00:20:13 -0600

    Verificador

--- src/app/pages/rifas_lobo/verificador/verificador.component.ts
+++ src/app/pages/rifas_lobo/verificador/verificador.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import {backend_url, swalErrorHttpResponse} from "../../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import {forceAutocomplete} from "@angular/cli/src/utilities/environment-options";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-verificador",
@@ -7,8 +11,21 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 export class VerificadorComponent implements OnInit, OnDestroy {
   isCollapsed = true;
   isMobile: boolean;
+  participantes: any[] = [];
 
-  constructor() {}
+  data = {
+    name: ""
+  }
+
+  final_data = {
+    id_participante : '',
+    tickets : "",
+    folio : '',
+    amount : '',
+    payment_method : "",
+  }
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.isMobile = window.innerWidth < 768;
@@ -23,5 +40,50 @@ export class VerificadorComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.remove("verificador-page");
+  }
+
+  getParticipant() {
+    const form_data = new FormData();
+    form_data.append('name', this.data.name);
+
+    console.log(this.data.name);
+
+    this.http.post(`${backend_url}getParticipantByName`, form_data).subscribe(
+        (response) => {
+          console.log(response);
+          this.participantes = response['participantes'];
+        },
+        (error) => {
+          swalErrorHttpResponse(error);
+        }
+    );
+  }
+
+  addPayment() {
+    console.log(this.final_data);
+    const form_data = new FormData();
+    form_data.append("data", JSON.stringify(this.final_data));
+    this.http.post(`${backend_url}addPago`, form_data).subscribe(
+        (response) => {
+          console.log(response);
+          if(response['code'] == 200) {
+            Swal.fire({
+              title: "Pago",
+              icon: "success",
+              text: "Pago creado correctamente",
+            });
+          } else  {
+            Swal.fire({
+              title: "Pago",
+              icon: "error",
+              text: "No se pudo realizar el pago",
+            });
+          }
+        },
+        (error) => {
+          swalErrorHttpResponse(error);
+        }
+    );
+
   }
 }
