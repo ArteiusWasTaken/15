a71ce3a4292f739166e337e1f96d86782893378a
Adrian <sauladrian.arias@gmail.com>
2024-01-09 01:17:51 -0600

    Mis tickets

--- src/app/pages/rifas_lobo/mistickets/mistickets.component.ts
+++ src/app/pages/rifas_lobo/mistickets/mistickets.component.ts
@@ -1,7 +1,53 @@
-import { Component } from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {backend_url, swalErrorHttpResponse} from "../../../../environments/environment";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-mistickets",
   templateUrl: "./mistickets.component.html",
 })
-export class MisticketsComponent {}
+export class MisticketsComponent implements OnInit, OnDestroy {
+  isCollapsed = true;
+  isMobile: boolean;
+  tickets: any[] = [];
+
+  data = {
+    email: '',
+    pass: ''
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 768;
+
+    window.addEventListener("resize", () => {
+      this.isMobile = window.innerWidth < 768;
+    });
+
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.add("verificador-page");
+  }
+  ngOnDestroy() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("verificador-page");
+  }
+  getTickets() {
+    console.log(this.data);
+    const form_data = new FormData();
+
+    form_data.append("email", this.data.email);
+    form_data.append("pass", this.data.pass);
+
+    this.http.post(`${backend_url}misTickets`, form_data).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          swalErrorHttpResponse(error);
+        }
+    );
+
+  }
+}
